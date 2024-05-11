26740a2551533360ff6388db36250e6b3bdcd562
iHunterDev <wcgcodes@gmail.com>
2024-05-11 19:49:29 +0800

    chore(🚧): add daisyui and TailwindCss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
 import './App.css';
 import {Greet} from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+    // const [resultText, setResultText] = useState("Please enter your name below 👇");
+    // const [name, setName] = useState('');
+    // const updateName = (e) => setName(e.target.value);
+    // const updateResultText = (result) => setResultText(result);
 
     function greet() {
         Greet(name).then(updateResultText);
@@ -15,12 +13,9 @@ function App() {
 
     return (
         <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     )
 }
