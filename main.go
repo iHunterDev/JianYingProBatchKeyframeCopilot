a7cf37801bf3f958dfb4077d231a7d5cb62005b3
iHunterDev <wcgcodes@gmail.com>
2024-05-13 13:06:40 +0800

    chore(🚧): add logo and about info to mac config

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
 
 func main() {
 	// Create an instance of the app structure
@@ -17,7 +21,7 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "jianyingpro-batch-keyframe-copilot",
+		Title:  "JianyingPro Batch Keyframe Copilot",
 		Width:  375,
 		Height: 667,
 		AssetServer: &assetserver.Options{
@@ -27,6 +31,14 @@ func main() {
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
+		},
+
+		Mac: &mac.Options{
+			About: &mac.AboutInfo{
+				Title:   "JianyingPro Batch Keyframe Copilot",
+				Message: "Copyright © 2024 - All right reserved by @iHunterDev",
+				Icon:    icon,
+			},
 		},
 	})
 
